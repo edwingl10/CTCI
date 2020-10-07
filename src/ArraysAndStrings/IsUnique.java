289236df8bc69e81d5c0279ac0121c9445b4876c
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-07 15:33:50 -0700

    solution for isUnique
    - checks if a string has all unique characters

--- src/ArraysAndStrings/IsUnique.java
+++ src/ArraysAndStrings/IsUnique.java
@@ -1,9 +1,26 @@
 package ArraysAndStrings;
 
 public class IsUnique {
+	/*
+	 * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+	 */
+	public static boolean isUniqueChars(String str) {
+		if(str.length() > 128) {
+			return false;
+		}
+		
+		boolean[] char_set = new boolean[128];
+		for(int i=0; i<str.length(); i++) {
+			int val = str.charAt(i);
+			if(char_set[val]) return false;
+			char_set[val] = true;
+		}
+		return true;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("hello");
+		String test = "helo";
+		System.out.println(isUniqueChars(test));
 	}
 }
 
